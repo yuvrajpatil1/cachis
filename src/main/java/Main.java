31c0da8a92b985802eca99b2017da39e033a6cf3
Yuvraj Patil <yuvrajpatil12092004@gmail.com>
2025-07-31 05:32:04 +0530

    #RG2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -19,6 +20,9 @@ public class Main {
       serverSocket.setReuseAddress(true);
       // Wait for connection from client.
       clientSocket = serverSocket.accept();
+
+      OutputStream outputStream = clientSocket.getOutputStream();
+      outputStream.write("+PONG\r\n".getBytes());
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     } finally {
